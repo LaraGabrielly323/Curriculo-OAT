5a94e051660f23a9ca3172864c610bf1febdcf71
Lara Gabrielly <laragabriellycamposdearaujo@gmail.com>
2026-09-22 14:17:57 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("%c[AVISO DO SISTEMA]: Sujeito carregado com sucesso. Não olhe para trás.", "color: #ff0000; font-size: 14px; background: #000; padding: 5px;");
+});
